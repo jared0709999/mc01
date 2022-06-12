114f652aab82a218bdbddb306f50788a7109bcee
edgardegantea <edgar.degante.a@gmail.com>
2022-06-11 19:52:20 -0500

    modificacíón de Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -37,124 +37,142 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-          child: Column(
-
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Card(
-                child: Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const ListTile(
-            leading: Icon(Icons.album),
-            title: Text('The Enchanted Nightingale'),
-            subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+    return Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.lightBlue, Color.fromRGBO(0, 0, 255, 0.7)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            title: Text(widget.title),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              TextButton(
-                child: const Text('BUY'),
-                onPressed: () {/* ... */},
-              ),
-              const SizedBox(width: 8),
-              TextButton(
-                child: const Text('LISTEN'),
-                onPressed: () {/* ... */},
-              ),
-              const SizedBox(width: 8),
-            ],
-          ),
-        ],
-      ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        const ListTile(
+                          leading: Icon(Icons.album),
+                          title: Text('The Enchanted Nightingale'),
+                          subtitle: Text(
+                              'Music by Julie Gable. Lyrics by Sidney Stein.'),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            TextButton(
+                              child: const Text('BUY'),
+                              onPressed: () {
+                                /* ... */
+                              },
+                            ),
+                            const SizedBox(width: 8),
+                            TextButton(
+                              child: const Text('LISTEN'),
+                              onPressed: () {
+                                /* ... */
+                              },
+                            ),
+                            const SizedBox(width: 8),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-
-              ),
-              RaisedButton(
-                child: Text('Autor'),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/autores');
-                },
-              ),
-            ],
+                RaisedButton(
+                  child: Text('Autor'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/autores');
+                  },
+                ),
+              ],
+            ),
           ),
-        ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Memoria Colectiva'),
+          floatingActionButton: FloatingActionButton(
+            onPressed: null,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          drawer: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: [
+                DrawerHeader(
+                  decoration: const BoxDecoration(
+                    color: Colors.lightBlue,
+                    image: DecorationImage(
+                      image: AssetImage('images/tec.png'),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  child: Stack(
+                    children: const [
+                      Positioned(
+                        bottom: 8.0,
+                        left: 4.0,
+                        child: Text(
+                          "Memoria Colectiva",
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    'images/inicio.png',
+                  ),
+                  title: const Text('Inicio'),
+                  onTap: () {
+                    // Update the state of the app.
+                    // ...
+                  },
+                ),
+                ListTile(
+                  title: const Text('Autores'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/autores');
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.image),
+                  title: const Text('Fotografías'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/galeria');
+                  },
+                ),
+                ListTile(
+                  title: const Text('Lengua Materna'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/lenguamaterna');
+                  },
+                ),
+                ListTile(
+                  title: const Text('Información'),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/informacion');
+                  },
+                ),
+              ],
             ),
-            ListTile(
-              leading: Image.asset(
-                'images/inicio.png',
-              ),
-              title: const Text('Inicio'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Autores'),
-              onTap: () {
-                  Navigator.pushNamed(context, '/autores');
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.image),
-              title: const Text('Fotografías'),
-              onTap: () {
-                Navigator.pushNamed(context, '/galeria');
-              },
-            ),
-            ListTile(
-              title: const Text('Lengua Materna'),
-              onTap: () {
-                Navigator.pushNamed(context, '/lenguamaterna');
-              },
-            ),
-            ListTile(
-              title: const Text('Información'),
-              onTap: () {
-                Navigator.pushNamed(context, '/informacion');
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
-
-
-
 
 class AutoresScreen extends StatelessWidget {
   @override
@@ -174,8 +192,6 @@ class AutoresScreen extends StatelessWidget {
     );
   }
 }
-
-
 
 class GaleriaScreen extends StatelessWidget {
   @override
