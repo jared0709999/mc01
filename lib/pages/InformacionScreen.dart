a540cb8d58627ec309e2c3fa175c32223e856214
edgardegantea <edgar.degante.a@gmail.com>
2022-06-23 22:00:55 -0500

    Conversión de URL convencional a nuevo formato Uri

--- lib/pages/InformacionScreen.dart
+++ lib/pages/InformacionScreen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:link_text/link_text.dart';
 
 class InformacionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    String info =
+        "Memoria colectiva es un proyecto iniciado por Ingeniería Informática del Instituto Tecnológico Superior de Teziutlán. El objetivo es promover el intercambio de ideas y compartir material escrito";
+
+    Uri url = Uri.parse("https://memoriacolectiva.mapaches.info");
+    const _text = 'https://memoriacolectiva.mapaches.info';
+
     return Scaffold(
       appBar: AppBar(
           leading: const Icon(Icons.info),
@@ -11,13 +18,63 @@ class InformacionScreen extends StatelessWidget {
             style: TextStyle(color: Colors.white),
           )),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // ¡Regrese a la primera pantalla cuando la pulsen!
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
+          child: Padding(
+            padding: EdgeInsets.all(20),
+        child: Column(
+        children: <Widget>[
+          const Spacer(
+            flex: 20,
+          ),
+          const Text(
+            "Memoria Colectiva",
+            style: TextStyle(fontSize: 14),
+          ),
+          Text('$info', textAlign: TextAlign.justify, style: TextStyle(fontSize: 20)),
+          const Spacer(
+            flex: 5,
+          ),
+          const Text(
+            "Website",
+            style: TextStyle(fontSize: 14),
+          ),
+
+          LinkText('Texto',
+            textAlign: TextAlign.center,
+            // You can optionally handle link tap event by yourself
+            // onLinkTap: (url) => ...
+          ),
+
+          Text(
+            'LinkText Widget with trimmed URL parameters',
+            textAlign: TextAlign.center,
+            style: TextStyle().copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 12.0),
+          LinkText(
+            _text,
+            textAlign: TextAlign.center,
+            shouldTrimParams: true,
+          ),
+
+
+          Text('$url', textAlign: TextAlign.justify, style: TextStyle(fontSize: 20)),
+          const Spacer(
+            flex: 5,
+          ),
+          const Text(
+            'Desarrollador',
+            style: TextStyle(fontSize: 14),
+          ),
+          const Text(
+            'Jared del Rosario Rojas Juárez',
+            textAlign: TextAlign.justify,
+            style: TextStyle(fontSize: 20),
+          ),
+          const Spacer(
+            flex: 5,
+          ),
+        ],
+      ))),
     );
   }
 }
